9172978666f71e2b353ec1c6c6b9a3e8adb8f19f
efigenioluiz <hanzou0012@gmail.com>
2025-09-11 09:53:20 -0300

    feat: add freemarker template on project

--- src/main/java/com/efigenioluiz/App.java
+++ src/main/java/com/efigenioluiz/App.java
@@ -1,16 +1,23 @@
 package com.efigenioluiz;
 
+import freemarker.template.Configuration;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.template.JavalinFreemarker;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_34);
+        cfg.setClassForTemplateLoading(App.class, "/templates");
+        cfg.setDefaultEncoding("UTF-8");
+
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add(staticFiles -> {
                 staticFiles.directory = "/public";
                 staticFiles.location = Location.CLASSPATH;
+                config.fileRenderer(new JavalinFreemarker(cfg));
             });
         }).start(7070);
 
